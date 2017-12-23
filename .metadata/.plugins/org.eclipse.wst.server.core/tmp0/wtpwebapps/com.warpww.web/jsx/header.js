c29b40afc4bcf82392b58ea89db1bd18149c6bbf
Xiake <xiake@ansebbian.com>
2017-12-23 08:57:52 -0600

    2017.12.23.08.57
    Second commit to catch misc. project files.

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/com.warpww.web/jsx/header.js
+++
@@ -1,7 +0,0 @@
-document.write('<meta charset="utf-8" />');
-document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0" />');
-document.write(' ');
-//document.write('<link rel="stylesheet" href="cssx/bootstrap.css" />');
-//document.write('<link rel="stylesheet" href="cssx/site.css" />');
-//document.write('<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">');
-//document.write('<style>.mySlides {display:none;}</style>)');
